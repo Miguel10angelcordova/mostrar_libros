08306a90baae35e81bc54e8b08fb6e95645ed7ac
Miguel10angelcordova <cordovasanchezmiguelangel10@gmail.com>
2024-06-16 22:09:40 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Administrador</title>
+</head>
+<body>
+    <h1>Opciones</h1>
+    <a href="/admin/crear.php/">Regristrar un libro</a><br>
+    <a href="/admin/mostrar_libros_bd.php/">Mostrar_libros</a><br>
+<a href="">Editar un libro</a><br>
+<a href="">Eliminar un libro</a><br>
+<a href=""></a>
+</body>
+</html>
